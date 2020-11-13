b36874034d59d049049b9d28833eee3978829e72
hack <hack>
2020-11-12 16:45:06 -0800

    pseudo-code

--- test1.py
+++ test1.py
@@ -1,7 +1,17 @@
-""" In the function option1 we are payed 100$ per day for 10 days so that totals to 1000$ which we return
-In the function option2 we are payed 1$ on the first day and it doubles all the way to the 10th day so we sum up
-from 2^0 to 2^9 and return it
-Or main function determine which option is better by comparing the sums
- of fuctions option1 or option2 is better
- if option1 and option2 is the same print pays the same
- otherwise print option1 or option2 is better, it does this using if conditions."""
+""" function option1(): 
+        return 100*10
+    
+    function option2(): 
+        x = sum of 2^0 to 2^9
+        return x
+    
+    function main():
+        if option1() is equal to option2():
+            print option1 and option 2 pays the same..
+        
+        else if option1() greater than option2(): 
+            print option1 is better
+
+        else :
+            print option 2 is better
+"""
